59249be664b8c61bf7ac5bb6c2769644aca0febe
qind1609 <hainguyen.cdt17@gmail.com>
2026-07-23 00:40:24 +0700

    remove setup.sh

--- label_studio_ml/examples/yolo/run.sh
+++ label_studio_ml/examples/yolo/run.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
+set -e
 
-# setup.sh
-bash ./setup.sh
+if [ ! -f .env ]; then
+    echo "Please create a .env file with the following content:"
+    echo "LABEL_STUDIO_API_KEY=your_api_key"
+    echo "LABEL_STUDIO_HOST=http://your_label_studio_host:port"
+    exit 1
+fi
+
+if [ -f ".env" ]; then
+     echo "Exporting environment variables from .env file..."
+    echo "Contents of .env file:"
+    cat .env
+    set -a
+    . ./.env
+    set +a
+    echo "Environment variables from .env file have been exported."
+fi
 
 docker compose up --build
